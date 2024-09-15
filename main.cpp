e0cd1e71b85318ea8fc2d063499f9b0f48f38509
TotallyNotClaritin <cawisnie@terpmail.umd.edu>
2024-09-15 06:28:34 -0400

    Skeleton methods laid out, consulting Win32 API to figure out how to make the window.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool gameLost;
+
+void createGame()
+{
+	gameLost = False;
+}
+
+void Draw()
+{
+
+}
+
+void Control()
+{
+
+}
+
+void gameLogic()
+{
+
+}
+
+int main()
+{
+
+
+}
